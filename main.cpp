8be5d453c935ca78f1f5d70576ce4725709d2ef5
jhaasdijk <jasperhaasdijk@hotmail.com>
2016-11-08 13:48:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,27 +2,34 @@
 // @author Tiko Huizinga // s4460898
 
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+
+
 int main(){
-    
-    /// Input
-    /*
-        B denotes a black, light-weight leave
-        R denotes a red, heavy leave
-        (m1m2) denotes a rod with two sub mobiles m1 and m2
-        
-        Number of rods is bounded by 1500
-    */
-    
-    /// Output
-    /*
-        IF possible: A single integer
-            The minimal number of swaps needed to make the mobile balanced
-        ELSE: stdout
-            The string: "discard"
-    */
-    
+
+    string input;
+    cin >> input;
+
     return 0;
 }
+
+
+/// Input
+/*
+    B denotes a black, light-weight leave
+    R denotes a red, heavy leave
+    (m1m2) denotes a rod with two sub mobiles m1 and m2
+
+    Number of rods is bounded by 1500
+*/
+
+/// Output
+/*
+    IF possible: A single integer
+        The minimal number of swaps needed to make the mobile balanced
+    ELSE: stdout
+        The string: "discard"
+*/
