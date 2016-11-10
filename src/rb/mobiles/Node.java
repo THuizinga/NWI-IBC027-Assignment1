48ef19fb398ec58c0e44c116327377f5b6e49525
tonto <tonto@Tonto-Thinkpad-X220>
2016-11-10 11:33:50 +0100

    Test commit to check git setup netbeans

--- src/rb/mobiles/Node.java
+++ src/rb/mobiles/Node.java
@@ -4,6 +4,7 @@ package rb.mobiles;
  *
  * @author Tiko Huizinga - s4460898 - <t.huizinga@student.ru.nl>
  * @author Jasper Haasdijk - s4449754 - <j.haasdijk@student.ru.nl>
+ * @author test
  */
 
 public class Node {
