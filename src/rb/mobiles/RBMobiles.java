2ec025cd6000c8a0343a604e25f863b794918fd9
tonto <jasperhaasdijk@hotmail.com>
2016-11-14 11:38:19 +0100

    Fix style format

--- src/rb/mobiles/RBMobiles.java
+++ src/rb/mobiles/RBMobiles.java
@@ -5,15 +5,15 @@ package rb.mobiles;
  * @author Tiko Huizinga - s4460898 - <t.huizinga@student.ru.nl>
  * @author Jasper Haasdijk - s4449754 - <j.haasdijk@student.ru.nl>
  */
-public class RBMobiles {
+
+public class RBMobiles{
 
     /**
-     * @param args the command line arguments
-     */
-	public static void main(String[] args) {
-		String input = "(BR)";
-		Node n = new Node(input);
-		System.out.println(n.solve());
-	}
-    
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) {
+        String input = "(BR)";
+        Node n = new Node(input);
+        System.out.println(n.solve());
+    }
 }
