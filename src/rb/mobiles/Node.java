82665febf691a93b75369eb218acbf84c5e1e64a
Tiko Huizinga <thuizinga@cs.ru.nl>
2016-11-14 11:55:45 +0100

    Change variables to private. Use equals function to compare strings. Fix file format

--- src/rb/mobiles/Node.java
+++ src/rb/mobiles/Node.java
@@ -8,51 +8,52 @@ package rb.mobiles;
 
 public class Node {
 
-	private Node left;
-	private Node right;
-	private String value;
-        private int weight;
-        private int leafNodes;
-        
-	
-    public Node(String input) {		
-	if("B".equals(input) || "R".equals(input))
-	    value = input;
-	else{
-	    int split = inputSplitter(input);
-	    left = new Node(input.substring(1, split));
-	    right = new Node(input.substring(split+1, input.length()-1));
-	}
+    private Node left;
+    private Node right;
+    private String value;
+    private int weight;
+    private int leafNodes;
+
+    public Node(String input){		
+        if("B".equals(input) || "R".equals(input))
+            value = input;
+        else{
+            int split = inputSplitter(input);
+            left = new Node(input.substring(0, split));
+            right = new Node(input.substring(split+1, input.length()-1));
+        }
     }
 
-    public int solve() {
-
-	return 0;
+    public int solve(){
+        // TODO Auto-generated method stub
+        return 0;
     }
-	
-	/**
-	 * Calculate the value where the input string should be splitted  
-	 * in to the left and the right string by counting the brackets.
-	 * @return the index of the last character of the left string
-	 */
-	private int inputSplitter(String s){
-		int result = 0;
-		int brackets = 0;
-		while(brackets > 0 || result == 0){
-			char cur = s.charAt(result);
-			if(cur == '('){
-				brackets ++;
-				result ++;
-			}
-			else if(cur == ')'){
-				brackets --;
-				result ++;
-			}
-			else{
-				result ++;
-			}
-		}
-		return result--;
-	}
+
+    /**
+    * Calculate the value where the input string should be splitted  
+    * in to the left and the right string by counting the brackets.
+    * @return the index of the last character of the left string
+    */
+    private int inputSplitter(String s){
+        int result = 0;
+        int brackets = 0;
+
+        while(brackets > 0 || result == 0){
+            char cur = s.charAt(result);
+
+            if(cur == '('){
+                brackets ++;
+                result ++;
+            }
+            else if(cur == ')'){
+                brackets --;
+                result ++;
+            }
+            else{
+                result ++;
+            }
+        }
+        return result--;
+    }
 
 }
