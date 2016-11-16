6a56a2729fc3791cd3b79ad061c866dcf79e037a
TikoH <tikohuizinga@gmail.com>
2016-11-16 15:20:05 +0100

    :rage: :rage:

--- src/rb/mobiles/RBMobiles.java
+++ src/rb/mobiles/RBMobiles.java
@@ -6,24 +6,22 @@ import java.util.Scanner;
  * @author Tiko Huizinga - s4460898 - <t.huizinga@student.ru.nl>
  * @author Jasper Haasdijk - s4449754 - <j.haasdijk@student.ru.nl>
  */
-
 public class RBMobiles{
 
     /**
-    * @param args the command line arguments
-    */
+     * @param args the command line arguments
+     */
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         Node n = new Node(input);
-        int answer = n.solve();
 
-        if(answer < 0){
-            System.out.println("discard");
+        if(true){       // Some condition...
+            System.out.println(n.solve());
         }
         else{
-            System.out.println(answer);
+            System.out.println("discard");
         }
     }
 }
