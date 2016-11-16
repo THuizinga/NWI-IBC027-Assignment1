8fcd8c46aad3c0fd528741ff8197f70b706a4d8e
tonto <jasperhaasdijk@hotmail.com>
2016-11-16 15:26:17 +0100

    jhaasdijk branch

--- src/rb/mobiles/RBMobiles.java
+++ src/rb/mobiles/RBMobiles.java
@@ -3,6 +3,7 @@ package rb.mobiles;
 import java.util.Scanner;
 
 /**
+ * jhaasdijk branch
  * @author Tiko Huizinga - s4460898 - <t.huizinga@student.ru.nl>
  * @author Jasper Haasdijk - s4449754 - <j.haasdijk@student.ru.nl>
  */
